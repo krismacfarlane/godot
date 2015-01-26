5be07a838db23101638fffef4555aff3a175e85a
Kristen MacFarlane <krismacfarlane@gmail.com>
2015-01-26 08:47:57 -0500

    adds work from last week

--- w06/d04/student/fellowship/scripts/fellowship.js
+++ w06/d04/student/fellowship/scripts/fellowship.js
@@ -19,4 +19,19 @@ var buddies = [
 var lands = ['The Shire', 'Rivendell', 'Mordor'];
 var body = document.querySelector('body');
 
+var makeMiddleEarth;
+function makeMiddleEarth() {
+  // create a section tag with an id of middle-earth
+  var middleEarth = document.createElement("div");
+  middleEarth.setAttribute("id", "middle-earth");
+  body.appendChild(middleEarth);
 
+  // add each land as an article tag
+  var addingLands = document.createElement("a");
+  var selectLands = document.querySelectorAll(lands).addingLands;
+  document.querySelector("#middle-earth").appendChild(selectLands);
+  // inside each article tag include an h1 with the name of the land
+  // append middle-earth to your document body
+}
+
+makeMiddleEarth();
--- w06/d05/student/js_paint/scripts/main.js
+++ w06/d05/student/js_paint/scripts/main.js
@@ -3,4 +3,55 @@ var mouseDepressed = false;
 var body = document.querySelector("body");
 
 
+var paintbrush = function(event) {
+  event.target.classList.add("green");
+  }
+
+var unpaint = function(event) {
+  if (mouseDepressed === false) {
+  event.target.classList.remove("green");
+  }
+}
+
+var depressMouse = function(event) {
+  mouseDepressed = true;
+  }
+
+var unDepressMouse = function(event) {
+  mouseDepressed = false;
+  }
+
+var canvas = document.querySelector(".canvas")
+
+for (var i = 0; i < 805; i ++) {
+  var tiles = document.createElement("div");
+  tiles.setAttribute("class", "tile");
+  tiles.addEventListener("mouseover", paintbrush);
+  tiles.addEventListener("mouseout", unpaint);
+  canvas.appendChild(tiles);
+};
+
+canvas.addEventListener("mousedown", depressMouse);
+canvas.addEventListener("mouseup", unDepressMouse);
+
+var tiles = document.querySelectorAll(".tile");
+var resetting = document.querySelector(".reset");
+
+var getRid = function(){
+  for (var i=0;i<tiles.length;i++){
+  tiles[i].classList.remove("green");
+  }
+}
+
+resetting.addEventListener("click", getRid);
+
+var colors = ["red", "goldenrod", "blue"];
+var palette = document.querySelector(".color-palette")
+
+for (i = 0; i < colors.length; i ++) {
+  var colorDiv = document.createElement("div");
+  colorDiv.setAttribute("class", "droplet");
+  colorDiv.classList.add(colors[i]);
+  palette.appendChild(colorDiv);
+}
 
