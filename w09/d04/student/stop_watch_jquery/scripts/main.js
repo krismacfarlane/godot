58f78851460a106dbbfc05063cfa07d512ba3827
Kristen MacFarlane <krismacfarlane@gmail.com>
2015-02-12 10:43:22 -0500

    adds morning stopwatch exercise

--- w09/d04/student/stop_watch_jquery/scripts/main.js
+++ w09/d04/student/stop_watch_jquery/scripts/main.js
@@ -1,1 +1,34 @@
 console.log("main.js linked");
+
+var $reset = $("#reset"),
+    $start = $("#start"),
+    $pause = $("#pause"),
+    $timer = $("#timer");
+
+var count = 0,
+    timerActive = false,
+    time;
+
+
+$start.on("click", function(e) {
+  console.log("the start button was clicked");
+  timerActive = true;
+  time = setInterval(function() {$timer.text("Time elapsed: " + count++)}, 1000);
+});
+
+$reset.on("click", function(e) {
+  console.log("the reset button was clicked");
+  clearInterval(time);
+  $timer.text("Stop Watch");
+  count = 0;
+});
+
+$pause.on("click", function(e) {
+  console.log("the pause button was clicked");
+  if (timerActive) {
+    clearInterval(time);
+    timerActive = false;
+  } else {
+    time = setInterval(function() {$timer.text("Time elapsed: " + count++)}, 1000);
+  }
+});
